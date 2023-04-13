67ba59285c4b42de7aedd84a21edb9cdbce90328
Barbara <barbaracantunes@gmail.com>
2023-04-13 20:46:02 -0300

    Add exercicio 1 da semana 1 do Módulo 2

---
+++ Módulo 2/Semana1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    int firstNumber;
+    int secondNumber;
+    int sum;
+
+    Console.WriteLine("Informe o primeiro número:");
+    firstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo número:");
+    secondNumber = int.Parse(Console.ReadLine());
+
+    sum = firstNumber + secondNumber;
+
+    Console.WriteLine("A soma dos números é " + sum + " .");
+  }
+}
