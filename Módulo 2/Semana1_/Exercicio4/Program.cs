c28408be0a32df166d4145c22e699839f7d9b485
Barbara <barbaracantunes@gmail.com>
2023-04-13 22:14:25 -0300

    Add ex4 da semana 1 do modulo 2

--- Módulo 2/Semana1_/Exercicio4/Program.cs
+++ Módulo 2/Semana1_/Exercicio4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+  private static void Main(string[] args)
+  {
+    string marca;
+    string modelo;
+    double kmRodados;
+
+    Console.WriteLine("Qual a marca do veículo?");
+    marca = Console.ReadLine();
+    Console.WriteLine("Qual o modelo do veículo?");
+    modelo = Console.ReadLine();
+    Console.WriteLine("Quantos km rodados tem o veículo?");
+    kmRodados = double.Parse(Console.ReadLine());
+
+    if (kmRodados > 10000)
+    {
+      Console.WriteLine("O carro " + modelo + " da marca " + marca + " precisa fazer revisão.");
+    }
+    else
+    {
+      Console.WriteLine("O carro " + modelo + " da marca " + marca + " não precisa fazer revisão.");
+    }
+  }
+}
