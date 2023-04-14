6ac35fa381d83e17033acb21d608f73313d4a2dd
Barbara <barbaracantunes@gmail.com>
2023-04-13 21:36:09 -0300

    Add ex3 da semana1 do módulo 2

--- Módulo 2/Semana1_/Exercicio3/Program.cs
+++ Módulo 2/Semana1_/Exercicio3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+  private static void Main(string[] args)
+  {
+    string nome;
+    int idade;
+
+    Console.WriteLine("Qual o nome do paciente?");
+    nome = Console.ReadLine();
+    Console.WriteLine("Qual a idade do paciente?");
+    idade = int.Parse(Console.ReadLine());
+
+    if (idade > 60) {
+      Console.WriteLine("O paciente " + nome + " é maior de idade e é idoso.");
+    } else if (idade < 18) {
+      Console.WriteLine("O paciente " + nome + " é menor de idade.");
+    } else {
+      Console.WriteLine("O paciente " + nome + " é maior de idade.");
+    }
+  } 
+}
