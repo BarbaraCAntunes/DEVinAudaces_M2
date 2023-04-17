33c0b87fa66a6c02b4f4923136c3222b2039d240
Barbara <barbaracantunes@gmail.com>
2023-04-17 20:43:26 -0300

    Add ex 5 da semana02 do módulo 02

---
+++ Módulo 2/Semana1_/Exercicio5/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    int[] numeros = new int[4];
+
+    for (int i = 0; i < numeros.Length; i++)
+    {
+      Console.WriteLine("Digite o " + (i + 1) + "º numero:");
+      numeros[i] = int.Parse(Console.ReadLine());
+    }
+
+    Array.Sort(numeros);
+
+    for (int i = 0; i < numeros.Length; i++)
+    {
+      Console.WriteLine("Numero " + (i + 1) + ": " + numeros[i]);
+    }
+  }
+}
