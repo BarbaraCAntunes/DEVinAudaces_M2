097d0a5f2d65b8a35936bc2a7a543d8dd5e539e3
Barbara <barbaracantunes@gmail.com>
2023-04-17 22:02:40 -0300

    Add exercício 10 da semana 02 do módulo 02

--- Módulo 2/Semana1_/Exercicio10/Program.cs
+++ Módulo 2/Semana1_/Exercicio10/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        double resultado = 0;
+        char operacao;
+
+        Console.WriteLine("Olá! Digite o símbolo da operação que você deseja fazer:");
+        Console.WriteLine("'+' - Adição");
+        Console.WriteLine("'-' - Subtração");
+        Console.WriteLine("'*' - Multiplicação");
+        Console.WriteLine("'/' - Divisão");
+
+        operacao = Console.ReadLine()[0];
+
+        Console.WriteLine("Digite o primeiro número:");
+        double numero1;
+        if (double.TryParse(Console.ReadLine(), out numero1)) {
+            Console.WriteLine("Digite o segundo número:");
+            double numero2;
+            if (double.TryParse(Console.ReadLine(), out numero2)) {
+                switch (operacao)
+                {
+                    case '+':
+                        resultado = numero1 + numero2;
+                        break;
+                    case '-':
+                        resultado = numero1 - numero2;
+                        break;
+                    case '*':
+                        resultado = numero1 * numero2;
+                        break;
+                    case '/':
+                        resultado = numero1 / numero2;
+                        break;
+                    default:
+                        Console.WriteLine("Digite uma operação válida!");
+                        break;
+                }
+                Console.WriteLine("O resultado da sua operação é: " + resultado);
+            } else {
+                Console.WriteLine("Digite um número válido!");
+            }
+        } else {
+            Console.WriteLine("Digite um número válido!");
+        }
+    }
+}
--- Módulo 2/Semana1_/Exercicio9/Program.cs
+++ Módulo 2/Semana1_/Exercicio9/Program.cs
@@ -1,21 +1,29 @@
-﻿int quantidadeNumeros;
-int soma = 0;
-double media;
+﻿internal partial class Program
+{
+  private static void Main(string[] args)
+  {
+    int quantidadeNumeros;
+    int soma = 0;
+    double media;
 
-Console.WriteLine("Quantos números você vai inserir?");
-quantidadeNumeros = int.Parse(Console.ReadLine());
+    Console.WriteLine("Quantos números você vai inserir?");
+    quantidadeNumeros = int.Parse(Console.ReadLine());
 
-int[] numeros = new int[quantidadeNumeros];
+    int[] numeros = new int[quantidadeNumeros];
 
-for (int i = 0; i < numeros.Length; i++){
-  Console.WriteLine("Digite o " + (i + 1) + "º número:");
-  numeros[i] = int.Parse(Console.ReadLine());
+    for (int i = 0; i < numeros.Length; i++)
+    {
+      Console.WriteLine("Digite o " + (i + 1) + "º número:");
+      numeros[i] = int.Parse(Console.ReadLine());
+    }
+
+    foreach (int numero in numeros)
+    {
+      soma += numero;
+    }
+
+    media = (double)soma / numeros.Length;
+
+    Console.WriteLine("A média artimética dos valores é: " + media);
+  }
 }
-
-foreach (int numero in numeros) {
-  soma += numero;
-}
-
-media = (double)soma / numeros.Length;
-
-Console.WriteLine("A média artimética dos valores é: " + media);
