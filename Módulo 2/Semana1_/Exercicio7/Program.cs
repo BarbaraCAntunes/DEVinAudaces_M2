1a996bd9fe2950656c77d9d833746e56e563ef81
Barbara <barbaracantunes@gmail.com>
2023-04-17 21:15:04 -0300

    Add exerício7 da semana02 do módulo02

--- Módulo 2/Semana1_/Exercicio7/Program.cs
+++ Módulo 2/Semana1_/Exercicio7/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+  private static void Main(string[] args)
+  {
+    int numero;
+
+    Console.WriteLine("Insira um número inteiro e positivo:");
+    numero = int.Parse(Console.ReadLine());
+
+    while (numero < 0)
+    {
+      Console.WriteLine("Informe apenas números positivos:");
+      numero = int.Parse(Console.ReadLine());
+    }
+
+    for (int i = 0; i <= numero; i += 2)
+    {
+      if (i % 2 == 0)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
