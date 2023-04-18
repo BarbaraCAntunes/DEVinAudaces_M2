298cb85934e71d75ca8d3135aedd6ad5cc48a57e
Barbara <barbaracantunes@gmail.com>
2023-04-17 21:38:40 -0300

    Add exercício 9 da semana 02 do módulo 02

--- Módulo 2/Semana1_/Exercicio9/Program.cs
+++ Módulo 2/Semana1_/Exercicio9/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int quantidadeNumeros;
+int soma = 0;
+double media;
+
+Console.WriteLine("Quantos números você vai inserir?");
+quantidadeNumeros = int.Parse(Console.ReadLine());
+
+int[] numeros = new int[quantidadeNumeros];
+
+for (int i = 0; i < numeros.Length; i++){
+  Console.WriteLine("Digite o " + (i + 1) + "º número:");
+  numeros[i] = int.Parse(Console.ReadLine());
+}
+
+foreach (int numero in numeros) {
+  soma += numero;
+}
+
+media = (double)soma / numeros.Length;
+
+Console.WriteLine("A média artimética dos valores é: " + media);
