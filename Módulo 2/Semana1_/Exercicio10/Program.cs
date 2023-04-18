563844d561e12517410431f7ff815aaab5d0f2d2
Barbara <barbaracantunes@gmail.com>
2023-04-17 22:13:37 -0300

    Atualiza

--- Módulo 2/Semana1_/Exercicio10/Program.cs
+++ Módulo 2/Semana1_/Exercicio10/Program.cs
@@ -1,48 +1,63 @@
 ﻿internal partial class Program
 {
-    private static void Main(string[] args)
+  private static void Main(string[] args)
+  {
+    double resultado = 0;
+    string operacao = "";
+
+    while (operacao != "S")
     {
-        double resultado = 0;
-        char operacao;
+      Console.WriteLine("Olá! Digite o símbolo da operação que você deseja fazer:");
+      Console.WriteLine("'+' - Adição");
+      Console.WriteLine("'-' - Subtração");
+      Console.WriteLine("'*' - Multiplicação");
+      Console.WriteLine("'/' - Divisão");
+      Console.WriteLine("'S' - Sair");
 
-        Console.WriteLine("Olá! Digite o símbolo da operação que você deseja fazer:");
-        Console.WriteLine("'+' - Adição");
-        Console.WriteLine("'-' - Subtração");
-        Console.WriteLine("'*' - Multiplicação");
-        Console.WriteLine("'/' - Divisão");
+      operacao = Console.ReadLine();
 
-        operacao = Console.ReadLine()[0];
+      if (operacao == "S")
+      {
+        Console.WriteLine("Obrigada por utilizar a calculadora!");
+        break;
+      }
 
-        Console.WriteLine("Digite o primeiro número:");
-        double numero1;
-        if (double.TryParse(Console.ReadLine(), out numero1)) {
-            Console.WriteLine("Digite o segundo número:");
-            double numero2;
-            if (double.TryParse(Console.ReadLine(), out numero2)) {
-                switch (operacao)
-                {
-                    case '+':
-                        resultado = numero1 + numero2;
-                        break;
-                    case '-':
-                        resultado = numero1 - numero2;
-                        break;
-                    case '*':
-                        resultado = numero1 * numero2;
-                        break;
-                    case '/':
-                        resultado = numero1 / numero2;
-                        break;
-                    default:
-                        Console.WriteLine("Digite uma operação válida!");
-                        break;
-                }
-                Console.WriteLine("O resultado da sua operação é: " + resultado);
-            } else {
-                Console.WriteLine("Digite um número válido!");
-            }
-        } else {
-            Console.WriteLine("Digite um número válido!");
+      Console.WriteLine("Digite o primeiro número:");
+      double numero1;
+      if (double.TryParse(Console.ReadLine(), out numero1))
+      {
+        Console.WriteLine("Digite o segundo número:");
+        double numero2;
+        if (double.TryParse(Console.ReadLine(), out numero2))
+        {
+          switch (operacao)
+          {
+            case "+":
+              resultado = numero1 + numero2;
+              break;
+            case "-":
+              resultado = numero1 - numero2;
+              break;
+            case "*":
+              resultado = numero1 * numero2;
+              break;
+            case "/":
+              if (numero2 == 0)
+              {
+                Console.WriteLine("Não é possível realizar divisões por 0!");
+              }
+              else
+              {
+                resultado = numero1 / numero2;
+              }
+              break;
+            default:
+              Console.WriteLine("Digite uma operação válida!");
+              break;
+          }
+          Console.WriteLine("O resultado da sua operação é: " + resultado);
         }
+      }
     }
+  }
 }
