a9b0bd5f352fd3069f7f1aa8a064d7f27d2b8b9b
Barbara <barbaracantunes@gmail.com>
2023-04-17 21:26:08 -0300

    Add exerício8 da semana2 do módulo2

--- Módulo 2/Semana1_/Exercicio8/Program.cs
+++ Módulo 2/Semana1_/Exercicio8/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+  private static void Main(string[] args)
+  {
+    int[] numeros = new int[5];
+    int maximo = 0;
+
+    for (int i = 0; i < numeros.Length; i++)
+    {
+      Console.WriteLine("Digite o " + (i + 1) + "º número:");
+      numeros[i] = int.Parse(Console.ReadLine());
+    }
+
+    maximo = numeros[0];
+    for (int i = 1; i < numeros.Length; i++)
+    {
+      if (numeros[i] > maximo)
+      {
+        maximo = numeros[i];
+      }
+    }
+
+    Console.WriteLine("O maior número no array é: " + maximo);
+  }
+}
