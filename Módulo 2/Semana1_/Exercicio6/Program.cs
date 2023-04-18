aca70fa4d12768c78248167f530607a37f970f04
Barbara <barbaracantunes@gmail.com>
2023-04-17 21:03:06 -0300

    Add exercicio6 da semana 02 do módulo 02

--- Módulo 2/Semana1_/Exercicio6/Program.cs
+++ Módulo 2/Semana1_/Exercicio6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+  private static void Main(string[] args)
+  {
+    string frase = "A linguagem de programação C# é muito poderosa";
+    List<string> palavras = new List<string>();
+    string[] partes = frase.Split(' ');
+
+    foreach (string palavra in partes)
+    {
+      palavras.Add(palavra);
+    }
+
+
+
+    Console.WriteLine(palavras.Last());
+  }
+}
