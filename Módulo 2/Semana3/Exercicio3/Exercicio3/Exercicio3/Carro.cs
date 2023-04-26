2c771dca6503714af40727e6de0d7a7b00347535
Barbara <barbaracantunes@gmail.com>
2023-04-26 20:08:31 -0300

    Add exercicio 5 da semana 3 do módulo 2

---
+++ Módulo 2/Semana3/Exercicio3/Exercicio3/Exercicio3/Carro.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercicio3
+{
+    internal class Carro
+    {
+        public string Marca;
+        public string Modelo;
+        public string Placa;
+        public string Cor;
+        public string Proprietario;
+
+        public Carro(string marca, string modelo, string placa, string cor, string proprietario)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Placa = placa;
+            Cor = cor;
+            Proprietario = proprietario;
+        }
+    }
+}
