249d1d897a617f6c1ec21af3b44487c98f5fe9a4
Barbara <barbaracantunes@gmail.com>
2023-04-26 19:54:27 -0300

    Add exercício 4 da semana 03 do módulo 02

--- Módulo 2/Semana3/Exercicio3/Exercicio3/Exercicio3/Program.cs
+++ Módulo 2/Semana3/Exercicio3/Exercicio3/Exercicio3/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Exercicio3;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Proprietario proprietario1 = new Proprietario("João da Silva", "00189765478", "48998852644");
+        Proprietario proprietario2 = new Proprietario("Maria Souza", "07582965922", "48997736211");
+        Proprietario proprietario3 = new Proprietario("Juliana Souza", "00961009071", "48998226299");
+    }
+}
