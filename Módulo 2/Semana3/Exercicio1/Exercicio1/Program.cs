25dadae0a9597f26afa71c2e8f7bc03dc6431714
Barbara <barbaracantunes@gmail.com>
2023-04-26 15:59:55 -0300

    Add exercício 1 da semana 03 do módulo 2.

---
+++ Módulo 2/Semana3/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exercicio1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Insira um número inteiro:");
+        int numero1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insira outro número inteiro:");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        Sum sum = new Sum(numero1, numero2);
+
+        Console.WriteLine("SOMA: " + sum.CalcularSoma());
+    }
+}
---
+++ Módulo 2/Semana3/Exercicio1/Exercicio1/Sum.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio1
+{
+    internal class Sum
+    {
+
+        public int Numero1;
+        public int Numero2;
+
+        public Sum(int numero1, int numero2)
+        {
+            Numero1 = numero1;
+            Numero2 = numero2;
+        }
+
+        public int CalcularSoma()
+        {
+            return Numero1 + Numero2;
+        }
+
+    }
+}
