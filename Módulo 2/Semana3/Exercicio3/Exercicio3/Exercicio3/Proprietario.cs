b0e205bb28b9447596007597fc956459feb26ad9
Barbara <barbaracantunes@gmail.com>
2023-04-26 19:40:23 -0300

    Add exercício 3 da semana 3 do módulo 2

---
+++ Módulo 2/Semana3/Exercicio3/Exercicio3/Exercicio3/Proprietario.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio3
+{
+    internal class Proprietario
+    {
+        public string Nome;
+        public string CPF;
+        public string Telefone;
+
+        public Proprietario(string nome, string cpf, string telefone)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Telefone = telefone;
+        }
+    }
+}
