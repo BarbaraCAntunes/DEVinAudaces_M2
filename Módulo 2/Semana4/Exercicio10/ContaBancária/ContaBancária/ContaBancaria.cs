f7268b83235c606e5b740a495740dc61df8c82c3
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 20:32:31 -0300

    Add Ex 10 da semana 04

---
+++ Módulo 2/Semana4/Exercicio10/ContaBancária/ContaBancária/ContaBancaria.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace ContaBancária
+{
+    public class ContaBancaria
+    {
+        private decimal saldo;
+        private List<string> transacoes;
+
+        public ContaBancaria()
+        {
+            saldo = 0m;
+            transacoes = new List<string>();
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor > 0)
+            {
+                saldo += valor;
+                transacoes.Add($"Depósito de {valor}, Saldo atual: {saldo}");
+                Console.WriteLine("Depósito efetuado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("O valor do depósito precisa ser maior que zero.");
+            }
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= saldo && valor > 0)
+            {
+                saldo -= valor;
+                transacoes.Add($"Saque de {valor}, Saldo atual: {saldo}");
+                Console.WriteLine("Saque efetuado com sucesso!");
+
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente ou valor de saque inválido.");
+
+            }
+        }
+
+        public decimal VerificarSaldo()
+        {
+            return saldo;
+        }
+
+        public void VerificarTransacoes()
+        {
+            foreach (var transacao in transacoes)
+            {
+                Console.WriteLine(transacao);
+            };
+        }
+    }
+
+}
---
+++ Módulo 2/Semana4/Exercicio10/ContaBancária/ContaBancária/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ContaBancária
+{
+    public class Program
+    {
+        public static void Main()
+        {
+  
+            ContaBancaria conta = new ContaBancaria();
+
+            Console.WriteLine($"Saldo inicial: {conta.VerificarSaldo()}");
+
+            conta.Depositar(200);
+            conta.Depositar(300);
+
+            Console.WriteLine($"Saldo após depósitos: {conta.VerificarSaldo()}");
+
+
+            conta.Sacar(150);
+
+            Console.WriteLine($"Saldo após saque: {conta.VerificarSaldo()}");
+
+            Console.WriteLine("Transações realizadas:");
+            conta.VerificarTransacoes();
+            
+        }
+    }
+}
