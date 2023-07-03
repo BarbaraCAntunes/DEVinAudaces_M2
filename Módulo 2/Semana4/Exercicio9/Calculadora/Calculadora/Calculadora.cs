cd08ed9405f6b4029a26dbb6f77b511c83c489f0
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 20:22:10 -0300

    Add 9 da semana 04

---
+++ Módulo 2/Semana4/Exercicio9/Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Calculadora
+{
+    public static class Calculadora
+    {
+        public const double Pi = 3.14159265359;
+
+        public static void Soma(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static void Subtracao(double a, double b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        public static void Multiplicacao(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        public static void Divisao(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("O divisor não pode ser zero.");
+            }
+            Console.WriteLine(a / b);
+        }
+    }
+
+}
---
+++ Módulo 2/Semana4/Exercicio9/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculadora.Calculadora.Soma (5, 10);
+        Calculadora.Calculadora.Subtracao(20, 7);
+        Calculadora.Calculadora.Multiplicacao(4, 5);
+        Calculadora.Calculadora.Divisao(10, 2);
+    }
+}
