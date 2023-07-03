7ac9e1595a8fc957db4aaf8646478c9b0f8bb8ef
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 19:42:31 -0300

    Add ex 4 da semana 04

--- Módulo 2/Semana4/Exercicio2/Banco/Banco/ContaBancária.cs
+++ Módulo 2/Semana4/Exercicio2/Banco/Banco/ContaBancária.cs
@@ -4,11 +4,13 @@
     {
         private int _numeroConta;
         private Cliente _cliente;
+        private double _saldoConta;
 
         public ContaBancária(int numeroConta, Cliente cliente)
         {
             _numeroConta = numeroConta;
             _cliente = cliente;
+            _saldoConta = 0;
         }
 
         public void ExibirDados()
@@ -18,18 +20,29 @@
 
         public void Depositar(double valor)
         {
-            ;
+            _saldoConta += valor;
         }
 
-        public void Sacar(double valor)
+        public bool Saca(double valor)
         {
-            ;
+            if (valor <= _saldoConta)
+            {
+                _saldoConta -= valor;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
         }
 
-        public void getSaldo()
+        public void GetSaldo()
         {
-
+            double saldo = _saldoConta;
+            Console.WriteLine($"O saldo da sua conta é: {saldo}");
         }
+
 
 
         public override string ToString()
