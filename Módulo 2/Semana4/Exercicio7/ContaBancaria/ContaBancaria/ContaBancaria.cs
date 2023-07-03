56534eb2c9028446e6f2b55437293e2ff7273d6d
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 20:07:27 -0300

    Add 7 da semana 04

---
+++ Módulo 2/Semana4/Exercicio7/ContaBancaria/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,62 @@
+﻿public class ContaBancaria
+{
+    public string NumeroDaConta { get; set; }
+
+    private double _saldo;
+
+
+    public double Saldo
+    {
+        get { return _saldo; }
+        private set
+        {
+            if (value >= 0 && value <= ValorMaximoSaldo)
+            {
+                _saldo = value;
+            }
+            else
+            {
+                Console.WriteLine("O valor é inválido para o saldo.");
+            }
+        }
+    }
+
+    private readonly double ValorMaximoSaldo = 1000000.0;
+
+    public ContaBancaria(string numeroDaConta, double saldoInicial)
+    {
+        NumeroDaConta = numeroDaConta;
+        Saldo = saldoInicial;
+    }
+
+    public void Depositar(double valor)
+    {
+        if (valor > 0)
+        {
+            if (Saldo + valor <= ValorMaximoSaldo)
+            {
+                Saldo += valor;
+            }
+            else
+            {
+                Console.WriteLine("O depósito ultrapassará o valor máximo permitido de saldo.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("O valor do depósito deve ser maior que zero.");
+        }
+    }
+
+    public void Sacar(double valor)
+    {
+        if (Saldo >= valor)
+        {
+            Saldo -= valor;
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente para sacar esse valor.");
+        }
+    }
+}
---
+++ Módulo 2/Semana4/Exercicio7/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria("123456", 10000);
+        conta.Depositar(5000);
+        Console.WriteLine(conta.Saldo); 
+        conta.Sacar(2000);
+        Console.WriteLine(conta.Saldo); 
+    }
+}
