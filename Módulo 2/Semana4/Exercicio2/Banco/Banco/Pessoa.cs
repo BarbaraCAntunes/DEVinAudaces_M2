11d741fc85ced80dc7efffe56250a7d211e1ba38
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 19:55:09 -0300

    Add ex 5 da semana 04

---
+++ Módulo 2/Semana4/Exercicio2/Banco/Banco/Pessoa.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Banco
+{
+    internal class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public int Idade
+        {
+            get { return _idade; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _idade = value;
+                }
+                else
+                {
+                    Console.WriteLine("A idade não pode ser negativa!");
+                }
+            }
+        }
+
+        public void DefinirIdade(int value)
+        {
+            if (value >= 0)
+            {
+                Idade = value;
+            }
+            else
+            {
+                Console.WriteLine("A idade não pode ser negativa!");
+            }
+        }
+
+        public int RecuperarIdade()
+        {
+            return Idade;
+        }
+
+        public string RecuperarNome()
+        {
+            return Nome;
+        }
+    }
+}
