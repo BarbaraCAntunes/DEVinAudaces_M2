4ee3de8f96f330c58b85bf6d8e935aaffcc391f4
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 18:41:03 -0300

    Add ex 1 da semana 04

---
+++ Módulo 2/Semana4/Exercício1/Exercicio1/Exercicio1/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Exercicio1
+{
+    internal class Pessoa
+    {
+        private string _Nome;
+        protected int Idade;
+        public string Endereco;
+
+        public Pessoa(string nome, int idade, string endereco)
+        {
+            _Nome = nome;
+            Idade = idade;
+            Endereco = endereco;
+        }
+
+        public void Saudacao()
+        {
+            Console.WriteLine($"Olá, meu nome é {_Nome} e eu tenho {Idade} anos!");
+        }
+    }
+}
---
+++ Módulo 2/Semana4/Exercício1/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Exercicio1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa pessoa1 = new Pessoa("Barbara", 31, "Rua Girassol");
+
+        pessoa1.Saudacao();
+    }
+}
