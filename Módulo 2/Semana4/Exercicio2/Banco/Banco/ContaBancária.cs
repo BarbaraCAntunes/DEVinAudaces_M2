1feab83ad9f25a191e7c41c20153a20c0bccb264
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 19:06:28 -0300

    Add exercicio 2 da semana 04

---
+++ Módulo 2/Semana4/Exercicio2/Banco/Banco/ContaBancária.cs
@@ -0,0 +1,39 @@
+﻿namespace Banco
+{
+    internal class ContaBancária
+    {
+        private int _numeroConta;
+        private double _saldoConta;
+
+        public ContaBancária(int numeroConta)
+        {
+            _numeroConta = numeroConta;
+            _saldoConta = 0;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldoConta += valor;
+        }
+
+        public void Saca(double valor)
+        {
+            if (valor <= _saldoConta)
+            {
+                _saldoConta -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+        }
+
+        public void GetSaldo()
+        {
+            double saldo = _saldoConta;
+            Console.WriteLine($"O saldo da sua conta é: {saldo}");
+        }
+
+
+    }
+}
---
+++ Módulo 2/Semana4/Exercicio2/Banco/Banco/Program.cs
@@ -0,0 +1,13 @@
+﻿using Banco;
+
+ContaBancária conta1 = new(123);
+Console.WriteLine("Saldo de abertura da conta:");
+conta1.GetSaldo();
+Console.WriteLine("Deposita 1000.");
+conta1.Depositar(1000.00);
+Console.WriteLine("Saldo depois do depósito:");
+conta1.GetSaldo();
+Console.WriteLine("Saca 500.");
+conta1.Saca(500.00);
+Console.WriteLine("Saldo depois do saque:");
+conta1.GetSaldo();
