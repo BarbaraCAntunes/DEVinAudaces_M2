b304b983cffae3518afdfb0d81aee78dfe933d90
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 20:00:09 -0300

    Add ex 6 da semana 04

---
+++ Módulo 2/Semana4/Exercício6/Retangulo/Retangulo/Retangulo.cs
@@ -0,0 +1,42 @@
+﻿public class Retangulo
+{
+    private double _largura;
+    private double _altura;
+
+    public double Largura
+    {
+        get { return _largura; }
+        set
+        {
+            if (value >= 0)
+            {
+                _largura = value;
+            }
+            else
+            {
+                Console.WriteLine("Largura não pode ser negativa!");
+            }
+        }
+    }
+
+    public double Altura
+    {
+        get { return _altura; }
+        set
+        {
+            if (value >= 0)
+            {
+                _altura = value;
+            }
+            else
+            {
+                Console.WriteLine("Altura não pode ser negativa!");
+            }
+        }
+    }
+
+    public double CalcularArea()
+    {
+        return Largura * Altura;
+    }
+}
