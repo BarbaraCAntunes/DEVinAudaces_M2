dc862f068728b255a7c0af58de17dc1cbec52a0b
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 22:09:26 -0300

    Add ex 4 da semana 05

--- Módulo 2/Semana5/Semana05/Semana05/ClientService.cs
+++ Módulo 2/Semana5/Semana05/Semana05/ClientService.cs
@@ -1,12 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Semana05
 {
-    internal class ClientService
+    public class ClienteService
     {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public void CriarConta()
+        {
+            Console.WriteLine("Deseja criar uma conta para Pessoa Física ou Jurídica? (F/J)");
+            var resposta = Console.ReadLine();
+
+            if (resposta.ToUpper() == "F")
+            {
+                CriarContaPessoaFisica();
+            }
+            else if (resposta.ToUpper() == "J")
+            {
+                CriarContaPessoaJuridica();
+            }
+        }
+
+        private void CriarContaPessoaFisica()
+        {
+            Console.WriteLine("Informe o número da conta:");
+            var numeroConta = Console.ReadLine();
+
+            Console.WriteLine("Informe o endereço:");
+            var endereco = Console.ReadLine();
+
+            Console.WriteLine("Informe o CPF:");
+            var cpf = Console.ReadLine();
+
+            Console.WriteLine("Informe a data de nascimento (AAAA-MM-DD):");
+            var dataNascimento = DateTime.Parse(Console.ReadLine());
+
+            var cliente = new PessoaFisica
+            {
+                NumeroConta = numeroConta,
+                Endereco = endereco,
+                CPF = cpf,
+                DataNascimento = dataNascimento,
+                Saldo = 0
+            };
+
+            if (!cliente.EhMaior())
+            {
+                Console.WriteLine("Não é possível criar uma conta para uma pessoa menor de idade.");
+                return;
+            }
+
+            clientes.Add(cliente);
+            Console.WriteLine("Conta criada com sucesso!");
+        }
+
+        private void CriarContaPessoaJuridica()
+        {
+            Console.WriteLine("Informe o número da conta:");
+            var numeroConta = Console.ReadLine();
+
+            Console.WriteLine("Informe o endereço:");
+            var endereco = Console.ReadLine();
+
+            Console.WriteLine("Informe o CNPJ:");
+            var cnpj = Console.ReadLine();
+
+            var cliente = new PessoaJuridica
+            {
+                NumeroConta = numeroConta,
+                Endereco = endereco,
+                CNPJ = cnpj,
+                Saldo = 0
+            };
+
+            clientes.Add(cliente);
+            Console.WriteLine("Conta criada com sucesso!");
+        }
+
+        public Cliente BuscarClientePorNumeroDeConta(string numeroConta)
+        {
+            return clientes.FirstOrDefault(c => c.NumeroConta == numeroConta);
+        }
+
+        public void ExibirClientes()
+        {
+            foreach (var cliente in clientes)
+            {
+                cliente.ResumoCliente();
+                Console.WriteLine();
+            }
+        }
     }
 }
--- Módulo 2/Semana5/Semana05/Semana05/Program.cs
+++ Módulo 2/Semana5/Semana05/Semana05/Program.cs
@@ -1,79 +1,13 @@
-﻿using Semana05;
+﻿using System;
 
-public class Program
+namespace Semana05
 {
-
-    private static List<Cliente> clientes = new List<Cliente>();
-
-    public static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Deseja criar uma conta para Pessoa Física ou Jurídica? (F/J)");
-        var resposta = Console.ReadLine();
-
-        
-        if (resposta.ToUpper() == "F")
+        public static void Main(string[] args)
         {
-            CriarContaPessoaFisica();
+            ClienteService clienteService = new ClienteService();
+            clienteService.CriarConta();
         }
-        else if (resposta.ToUpper() == "J")
-        {
-            CriarContaPessoaJuridica();
-        } 
-    }
-
-    private static void CriarContaPessoaFisica()
-    {
-        Console.WriteLine("Informe o número da conta:");
-        var numeroConta = Console.ReadLine();
-
-        Console.WriteLine("Informe o endereço:");
-        var endereco = Console.ReadLine();
-
-        Console.WriteLine("Informe o CPF:");
-        var cpf = Console.ReadLine();
-
-        Console.WriteLine("Informe a data de nascimento (AAAA-MM-DD):");
-        var dataNascimento = DateTime.Parse(Console.ReadLine());
-
-        var cliente = new PessoaFisica
-        {
-            NumeroConta = numeroConta,
-            Endereco = endereco,
-            CPF = cpf,
-            DataNascimento = dataNascimento,
-            Saldo = 0
-        };
-
-        if (!cliente.EhMaior())
-        {
-            Console.WriteLine("Não é possível criar uma conta para uma pessoa menor de idade.");
-            return;
-        }
-
-        clientes.Add(cliente);
-        Console.WriteLine("Conta criada com sucesso!");
-    }
-
-    private static void CriarContaPessoaJuridica()
-    {
-        Console.WriteLine("Informe o número da conta:");
-        var numeroConta = Console.ReadLine();
-
-        Console.WriteLine("Informe o endereço:");
-        var endereco = Console.ReadLine();
-
-        Console.WriteLine("Informe o CNPJ:");
-        var cnpj = Console.ReadLine();
-
-        var cliente = new PessoaJuridica
-        {
-            NumeroConta = numeroConta,
-            Endereco = endereco,
-            CNPJ = cnpj,
-            Saldo = 0
-        };
-
-        clientes.Add(cliente);
-        Console.WriteLine("Conta criada com sucesso!");
     }
 }
