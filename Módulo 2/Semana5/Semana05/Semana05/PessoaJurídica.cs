22ce5a0b6d8646e2c6f2f97c88e550078845ad41
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 21:30:20 -0300

    Add ex 1 da semana 05

---
+++ Módulo 2/Semana5/Semana05/Semana05/PessoaJurídica.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Semana05
+{
+    public class PessoaJuridica : Cliente
+    {
+        public string CNPJ { get; set; }
+
+        public override void ResumoCliente()
+        {
+            base.ResumoCliente();
+            Console.WriteLine($"CNPJ: {CNPJ}");
+        }
+    }
+}
