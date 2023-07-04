f074478570b6d29b5a9897f93e58df5e11301076
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 23:12:26 -0300

    Add ex 6 da semana 06

---
+++ Módulo 2/Semana6/Exercicio5/Exercicio5/Exercicio5/ListaOrdenar.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Exercicio5
+{
+    internal class ListaOrdenar
+    {
+        public void OrdenarLista(List<int> lista)
+        {
+            int n = lista.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (lista[j] > lista[j + 1])
+                    {
+                        int temp = lista[j];
+                        lista[j] = lista[j + 1];
+                        lista[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
--- Módulo 2/Semana6/Exercicio5/Exercicio5/Exercicio5/Program.cs
+++ Módulo 2/Semana6/Exercicio5/Exercicio5/Exercicio5/Program.cs
@@ -7,7 +7,8 @@ internal class Program
         ListaInversor inversor = new ListaInversor();
         List<int> minhaLista = new List<int> { 1, 2, 3, 4, 5 };
         Console.WriteLine("Lista original:");
-        foreach (int i in minhaLista) {
+        foreach (int i in minhaLista)
+        {
             Console.WriteLine(i);
         }
         inversor.InverterLista(minhaLista);
@@ -17,5 +18,22 @@ internal class Program
             Console.WriteLine(i);
         }
 
+
+        ListaOrdenar ordenador = new ListaOrdenar();
+        List<int> numeros = new List<int> { 5, 2, 8, 1, 4 };
+
+        Console.WriteLine("Antes da ordenação:");
+        foreach (int num in numeros)
+        {
+            Console.Write(num + " ");
+        }
+
+        ordenador.OrdenarLista(numeros);
+
+        Console.WriteLine("\nDepois da ordenação:");
+        foreach (int num in numeros)
+        {
+            Console.Write(num + " ");
+        }
     }
 }
