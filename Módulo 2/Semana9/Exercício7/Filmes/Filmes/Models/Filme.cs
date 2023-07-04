9ae4297733979e2d928cae6fdf4039f1c0539542
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-04 11:55:28 -0300

    Add ex 8 da semana 09

---
+++ Módulo 2/Semana9/Exercício7/Filmes/Filmes/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Filmes.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Duracao { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+
+        public Filme(int id, string nome, string duracao, string diretor, string genero)
+        {
+            Id = id;
+            Nome = nome;
+            Duracao = duracao;
+            Diretor = diretor;
+            Genero = genero;
+        }
+    }
+}
