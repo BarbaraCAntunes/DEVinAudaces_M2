ace11d5c0fa8a340a40b7635d205d7225fb0ab18
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 21:34:41 -0300

    Add ex 2 da semana 05

--- Módulo 2/Semana5/Semana05/Semana05/PessoaFisica.cs
+++ Módulo 2/Semana5/Semana05/Semana05/PessoaFisica.cs
@@ -1,14 +1,22 @@
 ﻿
+
 namespace Semana05
 {
     public class PessoaFisica : Cliente
     {
         public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
 
         public override void ResumoCliente()
         {
             base.ResumoCliente();
             Console.WriteLine($"CPF: {CPF}");
+            Console.WriteLine($"Data de Nascimento: {DataNascimento}");
+        }
+
+        public bool EhMaior()
+        {
+            return DateTime.Today.Year - DataNascimento.Year >= 18;
         }
     }
 }
