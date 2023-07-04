85efbed6b904053804c55ba8010251e572b71990
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 22:29:46 -0300

    Add ex 9 da semana 05

--- Módulo 2/Semana5/Semana05/Semana05/IClientService.cs
+++ Módulo 2/Semana5/Semana05/Semana05/IClientService.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Semana05
+﻿namespace Semana05
 {
-    internal class IClientService
+    public interface IClienteService
     {
+        void CriarConta();
+
+        Cliente BuscarClientePorNumeroDeConta(string numeroConta);
+
+        void ExibirClientes();
     }
 }
