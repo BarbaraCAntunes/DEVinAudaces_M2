edc3668d5375a772a82cc0ae8da4e9db3e4ea944
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 23:06:07 -0300

    Add ex 5 da semana 06

---
+++ Módulo 2/Semana6/Exercicio5/Exercicio5/Exercicio5/ListaInversor.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio5
+{
+    public class ListaInversor
+    {
+        public void InverterLista<T>(List<T> lista)
+        {
+            int n = lista.Count;
+            for (int i = 0; i < n / 2; i++)
+            {
+                T temp = lista[i];
+                lista[i] = lista[n - i - 1];
+                lista[n - i - 1] = temp;
+            }
+        }
+    }
+}
---
+++ Módulo 2/Semana6/Exercicio5/Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,21 @@
+﻿using Exercicio5;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ListaInversor inversor = new ListaInversor();
+        List<int> minhaLista = new List<int> { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Lista original:");
+        foreach (int i in minhaLista) {
+            Console.WriteLine(i);
+        }
+        inversor.InverterLista(minhaLista);
+        Console.WriteLine("Lista invertida:");
+        foreach (int i in minhaLista)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
