98eaf9cadac89466a0cc71cb52562b28bff12e80
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 22:53:08 -0300

    Add ex 4 da semana 06

---
+++ Módulo 2/Semana6/Exercicio1/MeuProjetoConsole/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace MeuProjetoConsole
+{
+    public class Calculadora
+    {
+        public int Soma(params int[] numeros)
+        {
+            int total = 0;
+            foreach (int numero in numeros)
+            {
+                total += numero;
+            }
+            return total;
+        }
+    }
+}
---
+++ Módulo 2/Semana6/Exercicio1/MeuProjetoTeste/CalculadoraTest.cs
@@ -0,0 +1,15 @@
+﻿using MeuProjetoConsole;
+
+namespace MeuProjetoTeste
+{
+    public class CalculadoraTest
+    {
+        [Test]
+        public void SomaFuncionaComNumerosNegativos()
+        {
+            Calculadora calculadora = new Calculadora();
+            int resultado = calculadora.Soma(-5, -3, -7);
+            Assert.AreEqual(-15, resultado);
+        }
+    }
+}
