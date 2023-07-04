263592e0188a31e204a1adb04aeb044d8139c174
Barbara Antunes <barbaracantunes@gmail.com>
2023-07-03 21:51:50 -0300

    Add ex 4 da semana 05

--- Módulo 2/Semana5/Semana05/Semana05/Program.cs
+++ Módulo 2/Semana5/Semana05/Semana05/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Semana05;
+
+public class Program
+{
+
+    private static List<Cliente> clientes = new List<Cliente>();
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Deseja criar uma conta para Pessoa Física ou Jurídica? (F/J)");
+        var resposta = Console.ReadLine();
+
+        if (resposta.ToUpper() == "F")
+        {
+            CriarContaPessoaFisica();
+        }
+        else if (resposta.ToUpper() == "J")
+        {
+            CriarContaPessoaJuridica();
+        }
+    }
+
+    private static void CriarContaPessoaFisica()
+    {
+        Console.WriteLine("Informe o número da conta:");
+        var numeroConta = Console.ReadLine();
+
+        Console.WriteLine("Informe o endereço:");
+        var endereco = Console.ReadLine();
+
+        Console.WriteLine("Informe o CPF:");
+        var cpf = Console.ReadLine();
+
+        Console.WriteLine("Informe a data de nascimento (AAAA-MM-DD):");
+        var dataNascimento = DateTime.Parse(Console.ReadLine());
+
+        var cliente = new PessoaFisica
+        {
+            NumeroConta = numeroConta,
+            Endereco = endereco,
+            CPF = cpf,
+            DataNascimento = dataNascimento,
+            Saldo = 0
+        };
+
+        if (!cliente.EhMaior())
+        {
+            Console.WriteLine("Não é possível criar uma conta para uma pessoa menor de idade.");
+            return;
+        }
+
+        clientes.Add(cliente);
+        Console.WriteLine("Conta criada com sucesso!");
+    }
+
+    private static void CriarContaPessoaJuridica()
+    {
+        Console.WriteLine("Informe o número da conta:");
+        var numeroConta = Console.ReadLine();
+
+        Console.WriteLine("Informe o endereço:");
+        var endereco = Console.ReadLine();
+
+        Console.WriteLine("Informe o CNPJ:");
+        var cnpj = Console.ReadLine();
+
+        var cliente = new PessoaJuridica
+        {
+            NumeroConta = numeroConta,
+            Endereco = endereco,
+            CNPJ = cnpj,
+            Saldo = 0
+        };
+
+        clientes.Add(cliente);
+        Console.WriteLine("Conta criada com sucesso!");
+    }
+}
